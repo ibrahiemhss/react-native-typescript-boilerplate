897e497d0b0cad9aded5283569b21584a1ffbe6c
Jafar Jabr <jafaronly@yahoo.com>
2020-05-26 00:14:43 +0300

    Update hamburgerButton.tsx

--- shared/components/buttons/hamburgerButton.tsx
+++ shared/components/buttons/hamburgerButton.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import GlobalLtrStyle from '../../styles/global.ltr.style';
 import * as NavigationService from '../../services/navigationService';
-import Icon from 'react-native-vector-icons/FontAwesome';
-
+import { Icon } from 'react-native-elements';
 
 const HamburgerButton = (): JSX.Element => (
   <Icon
-    name="bars"
+    name="menu"
     size={30}
     style={GlobalLtrStyle.HamburgerIcon}
     onPress={() => NavigationService.openDrawer()}
